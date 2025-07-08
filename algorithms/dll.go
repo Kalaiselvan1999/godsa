3b5b2238bf12d5a54171377fa03ca9e2c1585fcc
Kalaiselvan1999 <selvankalai638@gmail.com>
2025-07-08 21:59:56 +0530

    Changes

--- algorithms/dll.go
+++ algorithms/dll.go
@@ -230,3 +230,25 @@ func (dll *DoublyLinkedList) InsertAfterNode(data int, node *Node) {
 	}
 	node.Next = newNode
 }
+
+func LoadDll() {
+	dll := DoublyLinkedList{}
+	dll.ArrayToDLL([]int{10, 20, 30, 40})
+	dll.Display()
+
+	dll.InsertBeforeHead(5)
+	dll.InsertAfterHead(15)
+	dll.InsertAfterTail(50)
+	dll.InsertBeforeTail(45)
+	dll.InsertBeforeKth(2, 12)
+	dll.InsertAfterKth(3, 25)
+
+	dll.Display()
+
+	dll.DeleteHead()
+	dll.DeleteTail()
+	dll.DeleteKthElement(2)
+	dll.DeleteNode(25)
+
+	dll.Display()
+}
--- main.go
+++ main.go
@@ -4,23 +4,5 @@ import "github.com/Kalaiselvan1999/godsa/algorithms"
 
 
 func main() {
-	dll := &algorithms.DoublyLinkedList{}
-	dll.ArrayToDLL([]int{10, 20, 30, 40})
-	dll.Display()
-
-	dll.InsertBeforeHead(5)
-	dll.InsertAfterHead(15)
-	dll.InsertAfterTail(50)
-	dll.InsertBeforeTail(45)
-	dll.InsertBeforeKth(2, 12)
-	dll.InsertAfterKth(3, 25)
-
-	dll.Display()
-
-	dll.DeleteHead()
-	dll.DeleteTail()
-	dll.DeleteKthElement(2)
-	dll.DeleteNode(25)
-
-	dll.Display()
+	algorithms.LoadDll()
 }
